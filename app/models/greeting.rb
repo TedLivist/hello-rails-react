98683899c4911ffd4bcde92b14a62c1af9943930
TedLivist <livistteddy@gmail.com>
2021-11-09 11:08:57 +0100

    Validate Greeting and add seed code

--- app/models/greeting.rb
+++ app/models/greeting.rb
@@ -1,2 +1,5 @@
 class Greeting < ApplicationRecord
+
+  validates :message, presence: true, length: {minimum: 10}
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Greeting.find_or_create_by(message: 'Hello monsieur!')
+Greeting.find_or_create_by(message: 'Bonnuit Alejandro')
+Greeting.find_or_create_by(message: 'Bon apres midi mademoiselle')
+Greeting.find_or_create_by(message: 'Buenos dias Sergio!')
+Greeting.find_or_create_by(message: 'Buenas noches mi amor')
