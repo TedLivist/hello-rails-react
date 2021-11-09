8bb1b5312195879c4da1a57cb9629d16855b46f1
TedLivist <livistteddy@gmail.com>
2021-11-09 12:21:48 +0100

    Add react_components

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -1,13 +1,15 @@
 import React from "react"
 
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Greeting from "./Greeting";
 
 class App extends React.Component {
   render () {
     return (
       <BrowserRouter>
         <Routes>
-          <Route exact path="/" element="Home" />
+          <Route exact path="/" element="Home.....Home.....Hello!" />
+          <Route path="/greeting" element={<Greeting greeting="Hello, GReetings..."/>} />
         </Routes>
       </BrowserRouter>
     );
--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,11 @@ Rails.application.routes.draw do
       get 'greeting', to: 'greetings#index'
     end
   end
+
+  # Forward all requests to StaticController#index but requests
+  # must be non-Ajax(!req.xhr?) and HTML Mime type (req.format.html?).
+  # This does not include the root ("/") path.
+  get '*page', to: 'static#index', constraints: ->(req) do
+    !req.xhr? && req.format.html?
+  end
 end
